900e8d5849e14de0ff9e859d0c28c0e9eba07d0c
Matthew Jones <majones@nvidia.com>
2018-12-10 13:27:06 -0800

    FIX/ENH: fix readme, add CPU ARCH flag, add logging

--- utils/dask-setup.sh
+++ utils/dask-setup.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-export NCCL_P2P_DISABLE=1
-# export NCCL_SOCKET_IFNAME=ib
 
 export DASK_DISTRIBUTED__SCHEDULER__WORK_STEALING=False
 export DASK_DISTRIBUTED__SCHEDULER__BANDWIDTH=1
 
 ENVNAME=$1
-NWORKERS=$2
-DASK_SCHED_PORT=$3
-DASK_SCHED_BOKEH_PORT=$4
-DASK_WORKER_BOKEH_PORT=$5
-MASTER_IPADDR=$6
-WHOAMI=$7
-DEBUG=$8
+ARCH=$2
+NWORKERS=$3
+DASK_SCHED_PORT=$4
+DASK_SCHED_BOKEH_PORT=$5
+DASK_WORKER_BOKEH_PORT=$6
+MASTER_IPADDR=$7
+WHOAMI=$8
+LOG=$9
 
 DASK_LOCAL_DIR=./.dask
 NUM_GPUS=$(nvidia-smi --list-gpus | wc --lines)
@@ -24,75 +23,141 @@ echo -e "\n"
 
 echo "shutting down current dask cluster if it exists..."
 NUM_SCREENS=$(screen -list | grep --only-matching --extended-regexp '[0-9]\ Socket|[0-9]{1,10}\ Sockets' | grep --only-matching --extended-regexp '[0-9]{1,10}')
-SCREENS=($(screen -list | grep --only-matching --extended-regexp '[0-9]{1,10}\.dask|[0-9]{1,10}\.gpu' | grep --only-matching --extended-regexp '[0-9]{1,10}'))
-if [[ $NUM_SCREENS > 0 ]]; then
+SCREENS=($(screen -list | grep --only-matching --extended-regexp '[0-9]{1,10}\.dask|[0-9]{1,10}\.cpu|[0-9]{1,10}\.gpu' | grep --only-matching --extended-regexp '[0-9]{1,10}'))
+if [[ "$NUM_SCREENS" != "" ]]; then
     screen -wipe
-    for screen_id in $(seq 1 $NUM_SCREENS);
+    for screen_id in $(seq 1 $NUM_SCREENS)
     do
-    index=$screen_id-1
-    echo ${SCREENS[$index]}
-    screen -S ${SCREENS[$index]} -X quit
+        index=$screen_id-1
+        echo ${SCREENS[$index]}
+        screen -S ${SCREENS[$index]} -X quit
     done
 fi
 echo "... cluster shut down"
 
 echo -e "\n"
 
-if [[ "0" -lt "$NWORKERS" ]] && [[ "$NWORKERS" -le "$NUM_GPUS" ]]; then
+if [[ "$ARCH" = "GPU" ]]; then
 
-    if [[ "$WHOAMI" = "MASTER" ]]; then
-        echo "initializing dask scheduler..."
-        screen -dmS dask_scheduler bash -c "source activate $ENVNAME && dask-scheduler"
-        sleep 5
-        echo "... scheduler started"
+    if [[ "$NWORKERS" -ge "$NUM_GPUS" ]]; then
+        
+        echo -e "\n"
+        echo "[error] the number of workers must be less than or equal to the number of GPUs"
+        echo -e "\n"
+    
     fi
 
-    echo -e "\n"
+    if [[ "0" -lt "$NWORKERS" ]] && [[ "$NWORKERS" -le "$NUM_GPUS" ]]; then
 
-    echo "starting $NWORKERS worker(s)..."
-    declare -a WIDS
-    for worker_id in $(seq 1 $NWORKERS);
-    do
-    start=$(( worker_id - 1 ))
-    end=$(( NWORKERS - 1 ))
-    other=$(( start - 1 ))
-    devs=$(seq --separator=, $start $end)
-    second=$(seq --separator=, 0 $other)
-    if [ "$second" != "" ]; then
-        devs="$devs,$second"
+        if [[ "$WHOAMI" = "MASTER" ]]; then
+
+            echo "initializing dask scheduler..."
+            screen -dmS dask_scheduler bash -c "source activate $ENVNAME && dask-scheduler"
+            sleep 5
+            echo "... scheduler started"
+
+        fi
+
+        echo -e "\n"
+        echo "starting $NWORKERS worker(s)..."
+
+        for worker_id in $(seq 1 $NWORKERS)
+        do
+            start=$(( worker_id - 1 ))
+            end=$(( NWORKERS - 1 ))
+            other=$(( start - 1 ))
+            devs=$(seq --separator=, $start $end)
+            second=$(seq --separator=, 0 $other)
+            if [[ "$second" != "" ]]; then
+                devs="$devs,$second"
+            fi
+
+            echo "... starting gpu worker $worker_id"
+            
+            if [[ "$LOG" = "DEBUG" ]]; then
+                export NCCL_DEBUG=WARN
+                export create_worker="source activate $ENVNAME && \
+                                      cuda-memcheck dask-worker $MASTER_IPADDR:$DASK_SCHED_PORT \
+                                                                --host=${MY_IPADDR[0]} --no-nanny \
+                                                                --nprocs=1 --nthreads=1 \
+                                                                --memory-limit=0 --name ${MY_IPADDR[0]}_gpu_$worker_id \
+                                                                --local-directory $DASK_LOCAL_DIR/$name"
+                export logfile="${DASK_LOCAL_DIR}/${MY_IPADDR[0]}_gpu_${worker_id}_log.txt"
+                env CUDA_VISIBLE_DEVICES=$devs screen -dmS gpu_worker_$worker_id \
+                                                           bash -c 'script --flush --command "$create_worker" "$logfile"'
+            elif [[ "$LOG" = "INFO" ]]; then
+                export NCCL_DEBUG=INFO
+                export create_worker="source activate $ENVNAME && \
+                                      dask-worker $MASTER_IPADDR:$DASK_SCHED_PORT \
+                                                  --host=${MY_IPADDR[0]} --no-nanny \
+                                                  --nprocs=1 --nthreads=1 \
+                                                  --memory-limit=0 --name ${MY_IPADDR[0]}_gpu_$worker_id \
+                                                  --local-directory $DASK_LOCAL_DIR/$name"
+                export logfile="${DASK_LOCAL_DIR}/${MY_IPADDR[0]}_gpu_${worker_id}_log.txt"
+                env CUDA_VISIBLE_DEVICES=$devs screen -dmS gpu_worker_$worker_id \
+                                                           bash -c 'script --flush --command "$create_worker" "$logfile"'
+            else
+                export create_worker="source activate $ENVNAME && \
+                                      dask-worker $MASTER_IPADDR:$DASK_SCHED_PORT \
+                                                  --host=${MY_IPADDR[0]} --no-nanny \
+                                                  --nprocs=1 --nthreads=1 \
+                                                  --memory-limit=0 --name ${MY_IPADDR[0]}_gpu_$worker_id \
+                                                  --local-directory $DASK_LOCAL_DIR/$name"
+                env CUDA_VISIBLE_DEVICES=$devs screen -dmS gpu_worker_$worker_id \
+                                                           bash -c "$create_worker"
+            fi
+            
+        done
     fi
-    echo "... starting gpu worker $worker_id"
+fi
 
-    if [[ "$DEBUG" = "DEBUG" ]]; then
-        export create_worker="source activate $ENVNAME && \
-                              cuda-memcheck dask-worker $MASTER_IPADDR:$DASK_SCHED_PORT \
-                                                        --host=${MY_IPADDR[0]} --no-nanny \
-                                                        --nprocs=1 --nthreads=1 \
-                                                        --memory-limit=0 --name ${MY_IPADDR[0]}_gpu_$worker_id \
-                                                        --local-directory $DASK_LOCAL_DIR/$name"
-        export logfile="${DASK_LOCAL_DIR}/gpu_worker_${worker_id}_log.txt"
-        env CUDA_VISIBLE_DEVICES=$devs screen -dmS gpu_worker_$worker_id \
-                                                   bash -c 'script -c "$create_worker" "$logfile"'
-    else
-        export create_worker="source activate $ENVNAME && \
-                              dask-worker $MASTER_IPADDR:$DASK_SCHED_PORT \
-                                          --host=${MY_IPADDR[0]} --no-nanny \
-                                          --nprocs=1 --nthreads=1 \
-                                          --memory-limit=0 --name ${MY_IPADDR[0]}_gpu_$worker_id \
-                                          --local-directory $DASK_LOCAL_DIR/$name"
-        env CUDA_VISIBLE_DEVICES=$devs screen -dmS gpu_worker_$worker_id \
-                                                   bash -c "$create_worker"
+if [[ "$ARCH" = "CPU" ]]; then
+
+    if [[ "0" -le "$NWORKERS" ]]; then
+
+        if [[ "$WHOAMI" = "MASTER" ]]; then
+
+            echo "initializing dask scheduler..."
+            screen -dmS dask_scheduler bash -c "source activate $ENVNAME && dask-scheduler"
+            sleep 5
+            echo "... scheduler started"
+
+        fi
+
+        echo -e "\n"
+        echo "starting $NWORKERS worker(s)..."
+
+        for worker_id in $(seq 1 $NWORKERS)
+        do
+            echo "... starting cpu worker $worker_id"
+            if [[ "$LOG" = "DEBUG" ]]; then
+                echo -e "\n"
+                echo "[error] please set log-level to INFO. DEBUG log-level is only supported on GPU architectures"
+                echo -e "\n"
+                break
+            elif [[ "$LOG" = "INFO" ]]; then
+                export create_worker="source activate $ENVNAME && \
+                                      dask-worker $MASTER_IPADDR:$DASK_SCHED_PORT \
+                                                  --host=${MY_IPADDR[0]} --no-nanny \
+                                                  --nprocs=1 --nthreads=1 \
+                                                  --memory-limit=0 --name ${MY_IPADDR[0]}_cpu_$worker_id \
+                                                  --local-directory $DASK_LOCAL_DIR/$name"
+                export logfile="${DASK_LOCAL_DIR}/${MY_IPADDR[0]}_cpu_${worker_id}_log.txt"
+                screen -dmS cpu_worker_$worker_id \
+                                bash -c 'script --flush --command "$create_worker" "$logfile"'
+            else
+                export create_worker="source activate $ENVNAME && \
+                                      dask-worker $MASTER_IPADDR:$DASK_SCHED_PORT \
+                                                  --host=${MY_IPADDR[0]} --no-nanny \
+                                                  --nprocs=1 --nthreads=1 \
+                                                  --memory-limit=0 --name ${MY_IPADDR[0]}_cpu_$worker_id \
+                                                  --local-directory $DASK_LOCAL_DIR/$name"
+                screen -dmS cpu_worker_$worker_id \
+                                bash -c "$create_worker"
+            fi
+
+        done
     fi
-
-    WIDS[$id]=$!
-    done
-    sleep 5
-
-    echo -e "\n"
-
-    echo "... $NWORKERS worker(s) successfully started"
-
-    echo -e "\n"
 fi
 
 if [[ "$NWORKERS" -eq "0" ]]; then
