fbbe3a285cbbd85d9a079afaed2b96efa120a4a9
Raymond Douglass <ray@raydouglass.com>
2018-12-17 14:53:36 -0500

    BUG Fixes isues where split generates more than 100 files

--- utils/split_mortgage_data.sh
+++ utils/split_mortgage_data.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
-#Usage: ./split_mortgage_data.sh 500M
-#Adjust size as needed
 
 SRC="/raid/mortgage/perf_clean"
 DST="/raid/mortgage/perf_test"
+WORK="/raid/mortgage/"
+
+if [ $# -ne 1 ]; then
+    echo "Must supply size"
+    exit 1
+fi
 
 SIZE=$1
-
 
 function logger() {
   TS=`date`
@@ -33,21 +36,24 @@ function spill() {
 for i in `ls $SRC` ; do
   logger "Processing $i"
   logger "  Sorting..."
-  sort $SRC/$i > /tmp/$i
+  sort $SRC/$i > "$WORK/$i"
   logger "  Splitting..."
-  split -C $SIZE -d /tmp/$i
-  count=$(split --verbose --line-bytes=$SIZE -d /tmp/$i | wc -l)
+  files=$(split --verbose --line-bytes=$SIZE -d "$WORK/$i" | awk -F"'" '{print $2}')
+  count=`echo $files | awk '{print NF}'`
   logger "  Split into $count files"
-  rm -f /tmp/$i
-  for (( c=0; c<$count; c++)) do
-    printf -v pad "%02d" $c
-    mv x$pad $DST/${i}_$pad
+  rm -f "$WORK/$i"
+  s=${#count}
+  c=0
+  for file in $files ; do
+    printf -v pad "%0${s}d" $c
+    mv $file $DST/${i}_$pad
+    c=$(($c+1))
   done
   count=$(($count - 1))
   #spill $DST/${i}_0 $DST/${i}_1
   for (( c=0; c<$count; c++)) do
-    printf -v lpad "%02d" $c
-    printf -v rpad "%02d" $(($c+1))
+    printf -v lpad "%0${s}d" $c
+    printf -v rpad "%0${s}d" $(($c+1))
     left="$DST/${i}_$lpad"
     right="$DST/${i}_$rpad"
     spill $left $right
